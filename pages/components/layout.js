cb16e723735ee067ae8e51cba3adcb267519e81c
Christopher Pitt <cgpitt@gmail.com>
2020-01-30 20:45:39 +0200

    Add mono variant + change naming scheme

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -7,12 +7,17 @@ const Layout = function({ children }) {
                 <title>Use Noto</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" />
+                <link rel="stylesheet" href="/stylesheets/noto-sans-mono.css" />
                 <link rel="stylesheet" href="/stylesheets/noto-serif.css" />
             </Head>
             {children}
             <style jsx global>{`
-                * {
+                body {
                     font-family: 'Noto Serif';
+                }
+
+                code {
+                    font-family: 'Noto Sans Mono';
                 }
             `}</style>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,25 @@ const Index = function() {
             <p>To use these, import the stylesheets into your project:</p>
             {/* prettier-ignore */}
             <code className="block bg-gray-100 p-2 my-2 text-sm">
-                @import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-serif-regular.css'</span>;<br />
-                @import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-serif-italic.css'</span>;
+                <div>@import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-sans-mono-400.css'</span>;</div>
+                <div>@import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-serif-400-italic.css'</span>;</div>
+                <div>@import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-serif-400.css'</span>;</div>
             </code>
-            <p>...or you can import everything:</p>
+            <p>...or you can import everything per family:</p>
             {/* prettier-ignore */}
             <code className="block bg-gray-100 p-2 my-2 text-sm">
-                @import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-serif.css'</span>;
+                <div>@import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-sans-mono.css'</span>;</div>
+                <div>@import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-serif.css'</span>;</div>
+            </code>
+            <p>Then, use the fonts in your CSS:</p>
+            {/* prettier-ignore */}
+            <code className="block bg-gray-100 p-2 my-2 text-sm">
+                <div>body {`{`}</div>
+                <div className="ml-4">font-family: <span className="text-blue-500">'Noto Serif'</span>;</div>
+                <div>{`}`}</div>
+                <div className="mt-4">code {`{`}</div>
+                <div className="ml-4">font-family: <span className="text-blue-500">'Noto Sans Mono'</span>;</div>
+                <div>{`}`}</div>
             </code>
         </Layout>
     )
