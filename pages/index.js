1c15288725b2d62a76686acfd7f3139013363fdc
Christopher Pitt <cgpitt@gmail.com>
2020-01-30 20:15:11 +0200

    Remove needless brackets

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Index = function() {
     return (
         <Layout>
             <h1 className="text-lg font-semibold my-2">Use Noto!</h1>
-            <p>To use these, import the stylesheet(s) into your project:</p>
+            <p>To use these, import the stylesheets into your project:</p>
             {/* prettier-ignore */}
             <code className="block bg-gray-100 p-2 my-2 text-sm">
                 @import <span className="text-blue-500">'{process.env.DOMAIN}/stylesheets/noto-serif-regular.css'</span>;<br />
